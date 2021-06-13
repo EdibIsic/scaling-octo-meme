c07e4b21f9fb6bc6651c61cd829ca4e614059b68
GitHub Action <action@github.com>
2021-06-13 12:08:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/edibisic/scalingoctomeme/MyBundle.kt
+++ src/main/kotlin/com/github/edibisic/scalingoctomeme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.edibisic.scalingoctomeme
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/edibisic/scalingoctomeme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/edibisic/scalingoctomeme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.edibisic.scalingoctomeme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.edibisic.scalingoctomeme.MyBundle
 
 class MyApplicationService {
 
